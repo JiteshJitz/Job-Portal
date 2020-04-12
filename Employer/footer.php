91c6a42065d77fe80d3ebe884f5ddcc7fdbe3580
JITESH_RU <jiteshudaya18@gmail.com>
2020-04-13 03:54:23 +0530

    Update footer.php

--- Employer/footer.php
+++ Employer/footer.php
@@ -3,8 +3,8 @@
         <div id="top" class="noprint"><p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p></div>
         <hr class="noscreen" />
         
-        <p class="style1" id="createdby">created by Our team
+        <p class="style1" id="createdby">created by Jitesh
           <!-- DON´T REMOVE, PLEASE! -->
         </p>
-        <p> Hamro JOB.com</p>
+        <p> JOBPortal.com</p>
   </div> <!-- /footer -->
